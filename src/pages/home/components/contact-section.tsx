6cf2829ef8ab3dcd8e6d23760ef068074dac3e7c
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-29 00:49:47 -0300

    feat: add contact section component

---
+++ src/pages/home/components/contact-section.tsx
@@ -0,0 +1,21 @@
+import { Link, RevealAnimation } from "../../../components"
+
+export const ContactSection = () => {
+  return (
+    <section className="mb-10">
+      <RevealAnimation>
+        <p>Uau, você chegou até aqui?!</p>
+      </RevealAnimation>
+      <RevealAnimation>
+        <p>
+          Sua atenção e interesse significam muito para mim. Se quiser
+          acompanhar mais do meu trabalho ou entrar em contato, sinta-se à
+          vontade para me adicionar no{" "}
+          <Link href="https://linkedin.com/in/srdmatheus">LinkedIn</Link> e
+          conferir meus projetos no{" "}
+          <Link href="https://github.com/srdmatheus">GitHub</Link>.
+        </p>
+      </RevealAnimation>
+    </section>
+  );
+};
