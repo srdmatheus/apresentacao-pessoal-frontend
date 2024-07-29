ca1f9fd09b91782c9b3b5297973b7655d24f7c44
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-29 00:47:56 -0300

    chore: add custom classes

--- src/components/ui/link/index.tsx
+++ src/components/ui/link/index.tsx
@@ -1,18 +1,19 @@
 import { ArrowUpRightIcon } from "lucide-react";
 import { ComponentProps } from "react";
+import { cn } from "../../../lib/utils";
 
 type LinkProps = ComponentProps<'a'> & {
   href: string;
 }
 
-export const Link = ({ children, href, ...props }: LinkProps) => {
+export const Link = ({ children, href, className, ...props }: LinkProps) => {
   const isInternalLink = href.startsWith("/");
   return (
     <a
       href={href}
       target={isInternalLink ? "_self" : "_blank"}
       rel="noopener noreferrer"
-      className="inline-flex items-baseline font-bold text-accent underline hover:opacity-90"
+      className={cn("inline-flex items-baseline font-bold text-accent underline hover:opacity-90", className)}
       {...props}
     >
       {children}
