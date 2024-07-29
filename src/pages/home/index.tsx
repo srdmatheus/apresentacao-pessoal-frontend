ece939f62886fb7f6f0bee84ec0746f2b3726b64
Matheus Ribeiro <srdmatheus@gmail.com>
2024-07-29 00:50:08 -0300

    feat: add home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,31 @@
+import {
+  HeroSection,
+  AboutSection,
+  SkillsSection,
+  ContactSection,
+  ProjectsSection
+} from "./components"
+
+import { Divider } from "../../components"
+
 export const HomePage = () => {
   return (
-    <div className="text-xl">
-      <p>Home page</p>
-    </div>
+    <main className="mx-auto max-w-[1000px] px-4 flex flex-col">
+      <HeroSection />
+
+      <AboutSection />
+      
+      <Divider />
+
+      <SkillsSection />
+
+      <Divider />
+
+      <ProjectsSection />
+
+      <Divider />
+
+      <ContactSection />
+    </main>
   )
 }
